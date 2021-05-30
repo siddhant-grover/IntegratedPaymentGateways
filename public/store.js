4e538b531bd2aba8a01beeee110adfafbe86823d
Siddhant Grover <siddhantgroverboss@gmail.com>
2021-05-30 23:09:38 +0530

    reformatting

--- public/store.js
+++ public/store.js
@@ -1,186 +1,177 @@
-if (document.readyState == 'loading') {
-    document.addEventListener('DOMContentLoaded', ready)
+if (document.readyState == "loading") {
+  document.addEventListener("DOMContentLoaded", ready);
 } else {
-    ready()
+  ready();
 }
 
 function ready() {
-    var removeCartItemButtons = document.getElementsByClassName('btn-danger')
-    for (var i = 0; i < removeCartItemButtons.length; i++) {
-        var button = removeCartItemButtons[i]
-        button.addEventListener('click', removeCartItem)
-    }
+  var removeCartItemButtons = document.getElementsByClassName("btn-danger");
+  for (var i = 0; i < removeCartItemButtons.length; i++) {
+    var button = removeCartItemButtons[i];
+    button.addEventListener("click", removeCartItem);
+  }
 
-    var quantityInputs = document.getElementsByClassName('cart-quantity-input')
-    for (var i = 0; i < quantityInputs.length; i++) {
-        var input = quantityInputs[i]
-        input.addEventListener('change', quantityChanged)
-    }
+  var quantityInputs = document.getElementsByClassName("cart-quantity-input");
+  for (var i = 0; i < quantityInputs.length; i++) {
+    var input = quantityInputs[i];
+    input.addEventListener("change", quantityChanged);
+  }
 
-    var addToCartButtons = document.getElementsByClassName('shop-item-button')
-    for (var i = 0; i < addToCartButtons.length; i++) {
-        var button = addToCartButtons[i]
-        button.addEventListener('click', addToCartClicked)
-    }
+  var addToCartButtons = document.getElementsByClassName("shop-item-button");
+  for (var i = 0; i < addToCartButtons.length; i++) {
+    var button = addToCartButtons[i];
+    button.addEventListener("click", addToCartClicked);
+  }
 
-    document.getElementsByClassName('btn-purchase')[0].addEventListener('click', purchaseClicked)
+  document
+    .getElementsByClassName("btn-purchase")[0]
+    .addEventListener("click", purchaseClicked);
 }
 var stripe = Stripe(stripePublicKey);
 
-function purchaseClicked(e) { //change this so when we click on purchase button 
-    // alert('Thank you for your purchase')
-    // var cartItems = document.getElementsByClassName('cart-items')[0]
-    // while (cartItems.hasChildNodes()) {
-    //     cartItems.removeChild(cartItems.firstChild)
-    // }
-    // updateCartTotal()
-    priceElement = document.getElementsByClassName('cart-total-price')[0]
-    var amount = parseFloat((parseFloat(priceElement.innerText.replace('$', '')) * 100).toFixed(2)) //converting to cents
-    if(amount === 0){
-        alert("Buy some Products")
-        return;
-    }
-console.log(document.getElementById('gateways').value)
-if(document.getElementById('gateways').value==='Stripe'){
-    fetch("/create-checkout-session", {
-        method: "POST",
-        body:JSON.stringify({
-            amount,
-            currency:'USD',
-            name:'Generic Store Items',
-            image:'https://onlinecourses.one/wp-content/uploads/2019/08/Best-Adobe-After-Effects-Course-Class-Programs-Tutorial-Training-Certification-Online.jpg'
-            
-        }),
-        headers: {
-            "Content-Type": "application/json"
-          }
-    
+function purchaseClicked(e) {
+  //change this so when we click on purchase button
+  priceElement = document.getElementsByClassName("cart-total-price")[0];
+  var amount = parseFloat(
+    (parseFloat(priceElement.innerText.replace("$", "")) * 100).toFixed(2)
+  ); //converting to cents
+  if (amount === 0) {
+    alert("Buy some Products");
+    return;
+  }
+  if (document.getElementById("gateways").value === "Stripe") {
+    fetch("/stripe", {
+      method: "POST",
+      body: JSON.stringify({
+        amount,
+        currency: "USD",
+        name: "Generic Store Items",
+        image:
+          "https://onlinecourses.one/wp-content/uploads/2019/08/Best-Adobe-After-Effects-Course-Class-Programs-Tutorial-Training-Certification-Online.jpg",
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then(function (response) {
+        return response.json();
       })
-        .then(function (response) {
-          return response.json();
-        })
-        .then(function (session) {
-          return stripe.redirectToCheckout({ sessionId: session.id });
-        })
-        .then(function (result) {
-          // If redirectToCheckout fails due to a browser or network
-          // error, you should display the localized error message to your
-          // customer using error.message.
-          if (result.error) {
-            alert(result.error.message);
-          }
-        })
-        .catch(function (error) {
-          console.error("Error:", error);
-        });
-}
-if(document.getElementById('gateways').value==='Paypal'){
-    fetch("/pay", {
-        method: "POST",
-        body:JSON.stringify({
-            amount:parseFloat(amount/100).toFixed(2),
-            currency:'USD',
-            name:'Generic Store Items',
-            description:'Our products are high quality'
-        }),
-        headers: {
-            "Content-Type": "application/json"
-          }
-    
+      .then(function (session) {
+        return stripe.redirectToCheckout({sessionId: session.id});
       })
+      .then(function (result) {
+        // If redirectToCheckout fails due to a browser or network
+        // error, you should display the localized error message to your
+        // customer using error.message.
+        if (result.error) {
+          alert(result.error.message);
+        }
+      })
+      .catch(function (error) {
+        console.error("Error:", error);
+      });
+  }
+  if (document.getElementById("gateways").value === "Paypal") {
+    fetch("/paypal", {
+      method: "POST",
+      body: JSON.stringify({
+        amount: parseFloat(amount / 100).toFixed(2),
+        currency: "USD",
+        name: "Generic Store Items",
+        description: "Our products are high quality",
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
       .then((res) => {
-          return res.json()
+        return res.json();
       })
-      .then(data=>{
-        //console.log(data)
-        window.location = data.forwardLink
+      .then((data) => {
+        window.location = data.forwardLink;
       })
       .catch((err) => {
-        console.log(err)
-      })
+        console.log(err);
+      });
+  }
 
-}
-
-if(document.getElementById('gateways').value==='Razorpay'){
-    fetch('/order',{
-        method:'POST',//pass body values 
-        body:JSON.stringify({
-            amount:amount,
-            currency:'USD'
-
-        }),
-        headers:{"Content-Type":"application/json"}
-
+  if (document.getElementById("gateways").value === "Razorpay") {
+    fetch("/razorpay", {
+      method: "POST",
+      body: JSON.stringify({
+        amount: amount,
+        currency: "USD",
+      }),
+      headers: {"Content-Type": "application/json"},
     })
-    .then(resp=>resp.json())
-    .then(info=>{
+      .then((resp) => resp.json())
+      .then((info) => {
         var options = {
-    "key": KEY_ID, // Enter the Key ID generated from the Dashboard
-    // "amount": "50000", // Amount is in currency sub units. Default currency is INR. Hence, 50000 refers to 50000 paise
-    // "currency": "INR",
-    "name": "Music Store",
-    "description": "Test Transactions",
-    "image": "./images/Album 1.png",
-    "order_id": info.id, //This is a sample Order ID. Pass the `id` obtained in the response of Step 1
-    "callback_url": "/is-order-completed",//send a post req to this url
-    "prefill": {//info from ui - logged in user
-        "name": "Gaurav Kumar",
-        "email": "gaurav.kumar@example.com",
-        "contact": "9999999999"
-    },
-    "notes": {
-        "address": "Razorpay Corporate Office"
-    },
-    "theme": {
-        "color": "#000000"
-    }
-};
+          key: KEY_ID, // Enter the Key ID generated from the Dashboard
+          // "amount": "50000", // Amount is in currency sub units. Default currency is INR. Hence, 50000 refers to 50000 paise
+          // "currency": "INR",
+          name: "Music Store",
+          description: "Test Transactions",
+          image: "./images/Album 1.png",
+          order_id: info.id, //This is a sample Order ID. Pass the `id` obtained in the response of Step 1
+          callback_url: "/is-razorpay-order-completed", //send a post req to this url
+          prefill: {
+            //info from ui - logged in user
+            name: "Gaurav Kumar",
+            email: "gaurav.kumar@example.com",
+            contact: "9999999999",
+          },
+          notes: {
+            address: "Razorpay Corporate Office",
+          },
+          theme: {
+            color: "#000000",
+          },
+        };
 
-    var rzp1 = new Razorpay(options);
-    rzp1.open();
-    e.preventDefault();
-        
-        })
-
-}
+        var rzp1 = new Razorpay(options);
+        rzp1.open();
+        e.preventDefault();
+      });
+  }
 }
 
 function removeCartItem(event) {
-    var buttonClicked = event.target
-    buttonClicked.parentElement.parentElement.remove()
-    updateCartTotal()
+  var buttonClicked = event.target;
+  buttonClicked.parentElement.parentElement.remove();
+  updateCartTotal();
 }
 
 function quantityChanged(event) {
-    var input = event.target
-    if (isNaN(input.value) || input.value <= 0) {
-        input.value = 1
-    }
-    updateCartTotal()
+  var input = event.target;
+  if (isNaN(input.value) || input.value <= 0) {
+    input.value = 1;
+  }
+  updateCartTotal();
 }
 
 function addToCartClicked(event) {
-    var button = event.target
-    var shopItem = button.parentElement.parentElement
-    var title = shopItem.getElementsByClassName('shop-item-title')[0].innerText
-    var price = shopItem.getElementsByClassName('shop-item-price')[0].innerText
-    var imageSrc = shopItem.getElementsByClassName('shop-item-image')[0].src
-    addItemToCart(title, price, imageSrc)
-    updateCartTotal()
+  var button = event.target;
+  var shopItem = button.parentElement.parentElement;
+  var title = shopItem.getElementsByClassName("shop-item-title")[0].innerText;
+  var price = shopItem.getElementsByClassName("shop-item-price")[0].innerText;
+  var imageSrc = shopItem.getElementsByClassName("shop-item-image")[0].src;
+  addItemToCart(title, price, imageSrc);
+  updateCartTotal();
 }
 
 function addItemToCart(title, price, imageSrc) {
-    var cartRow = document.createElement('div')
-    cartRow.classList.add('cart-row')
-    var cartItems = document.getElementsByClassName('cart-items')[0]
-    var cartItemNames = cartItems.getElementsByClassName('cart-item-title')
-    for (var i = 0; i < cartItemNames.length; i++) {
-        if (cartItemNames[i].innerText == title) {
-            alert('This item is already added to the cart')
-            return
-        }
+  var cartRow = document.createElement("div");
+  cartRow.classList.add("cart-row");
+  var cartItems = document.getElementsByClassName("cart-items")[0];
+  var cartItemNames = cartItems.getElementsByClassName("cart-item-title");
+  for (var i = 0; i < cartItemNames.length; i++) {
+    if (cartItemNames[i].innerText == title) {
+      alert("This item is already added to the cart");
+      return;
     }
-    var cartRowContents = `
+  }
+  var cartRowContents = `
         <div class="cart-item cart-column">
             <img class="cart-item-image" src="${imageSrc}" width="100" height="100">
             <span class="cart-item-title">${title}</span>
@@ -189,25 +180,32 @@ function addItemToCart(title, price, imageSrc) {
         <div class="cart-quantity cart-column">
             <input class="cart-quantity-input" type="number" value="1">
             <button class="btn btn-danger" type="button">REMOVE</button>
-        </div>`
-    cartRow.innerHTML = cartRowContents
-    cartItems.append(cartRow)
-    cartRow.getElementsByClassName('btn-danger')[0].addEventListener('click', removeCartItem)
-    cartRow.getElementsByClassName('cart-quantity-input')[0].addEventListener('change', quantityChanged)
+        </div>`;
+  cartRow.innerHTML = cartRowContents;
+  cartItems.append(cartRow);
+  cartRow
+    .getElementsByClassName("btn-danger")[0]
+    .addEventListener("click", removeCartItem);
+  cartRow
+    .getElementsByClassName("cart-quantity-input")[0]
+    .addEventListener("change", quantityChanged);
 }
 
 function updateCartTotal() {
-    var cartItemContainer = document.getElementsByClassName('cart-items')[0]
-    var cartRows = cartItemContainer.getElementsByClassName('cart-row')
-    var total = 0
-    for (var i = 0; i < cartRows.length; i++) {
-        var cartRow = cartRows[i]
-        var priceElement = cartRow.getElementsByClassName('cart-price')[0]
-        var quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0]
-        var price = parseFloat(priceElement.innerText.replace('$', ''))
-        var quantity = quantityElement.value
-        total = total + (price * quantity)
-    }
-    total = Math.round(total * 100) / 100
-    document.getElementsByClassName('cart-total-price')[0].innerText = '$' + total
+  var cartItemContainer = document.getElementsByClassName("cart-items")[0];
+  var cartRows = cartItemContainer.getElementsByClassName("cart-row");
+  var total = 0;
+  for (var i = 0; i < cartRows.length; i++) {
+    var cartRow = cartRows[i];
+    var priceElement = cartRow.getElementsByClassName("cart-price")[0];
+    var quantityElement = cartRow.getElementsByClassName(
+      "cart-quantity-input"
+    )[0];
+    var price = parseFloat(priceElement.innerText.replace("$", ""));
+    var quantity = quantityElement.value;
+    total = total + price * quantity;
+  }
+  total = Math.round(total * 100) / 100;
+  document.getElementsByClassName("cart-total-price")[0].innerText =
+    "$" + total;
 }
--- server.js
+++ server.js
@@ -1,213 +1,198 @@
-if(process.env.NODE_ENV!=='production')//var set by node, tells us which environment are we on ,
-{
-    const dotenv = require("dotenv");
-    dotenv.config();
+if (process.env.NODE_ENV !== "production") {
+  const dotenv = require("dotenv");
+  dotenv.config();
 }
 
 const cors = require("cors");
-//so if we are in production we dont want to use env library 
-const stripeSecretKey = process.env.STRIPE_SECRET_KEY
-const stripePublicKey = process.env.STRIPE_PUBLIC_KEY
-const stripe = require('stripe')(stripeSecretKey);
-
-const Razorpay = require('razorpay')
-let KEY_ID= "rzp_test_qRDRpOoJR0OGFp"
-let KEY_SECRET = "pOCYTUxxeoTKVJxENaLMbmj2"
-const razorpay = new Razorpay({//initailize razorpay 
-    key_id:KEY_ID,
-    key_secret:KEY_SECRET
-})
-
-
-const express = require('express');
+const express = require("express");
 const app = express();
-const fs = require('fs')//allow us to read diff files 
+const fs = require("fs");
 app.use(express.json());
-app.use(express.urlencoded({extended:false}))
+app.use(express.urlencoded({extended: false}));
 app.use(cors());
 
-app.set('view engine','ejs')//ejs , front end use ejs , in order to render views
-app.use(express.static('public')) //where our static file gonna be
+app.set("view engine", "ejs");
+app.use(express.static("public"));
 
-app.get('/store',(req,res)=>{
-    fs.readFile('items.json',function(error,data){ //data is the info in the file
+const YOUR_DOMAIN = process.env.YOUR_DOMAIN;
 
-        if(error){
-            res.status(500).end()
-        }
-        else{
-            res.render('store.ejs',{//as we r using express by default all the views that are rendered with render method , need to live in a folder called views 
-                    items:JSON.parse(data),//passing items.json data in store.js as an items var 
-                    stripePublicKey:stripePublicKey,
-                    KEY_ID
-            })//render our store page , but as we want to use values from our server in the html page save it as ejs file
-        }
-    })
+const stripeSecretKey = process.env.STRIPE_SECRET_KEY;
+const stripePublicKey = process.env.STRIPE_PUBLIC_KEY;
+const stripe = require("stripe")(stripeSecretKey);
 
-})
+const Razorpay = require("razorpay");
+let KEY_ID = process.env.KEY_ID;
+let KEY_SECRET = process.env.KEY_SECRET;
+const razorpay = new Razorpay({
+  //initailize razorpay
+  key_id: KEY_ID,
+  key_secret: KEY_SECRET,
+});
 
+const paypal = require("paypal-rest-sdk");
+paypal.configure({
+  mode: "sandbox", //sandbox or live
+  client_id:process.env.CLIENT_ID,
+  client_secret:process.env.CLIENT_SECRET,
+});
 
-const YOUR_DOMAIN = 'http://localhost:3000'
-app.post('/create-checkout-session', async (req, res) => {
-    const session = await stripe.checkout.sessions.create({
-      payment_method_types: ['card'],
-      line_items: [
-        {
-          price_data: {
-            currency: req.body.currency,
-            product_data: {
-              name: req.body.name,
-              images: [req.body.image],
-            },
-            unit_amount: req.body.amount,
-          },
-          quantity: 1,
-        },
-      ],
-      mode: 'payment',
-      success_url: `${YOUR_DOMAIN}/stripe/success`,
-      cancel_url: `${YOUR_DOMAIN}/stripe/cancel`,
-    });
-  
-    res.json({ id: session.id });
-  });
+app.get("/store", (req, res) => {
+  fs.readFile("items.json", function (error, data) {
+    //data is the info in the file
 
-
-  //PAYPAL
-
-  const paypal = require('paypal-rest-sdk');
-  paypal.configure({
-    'mode': 'sandbox', //sandbox or live
-    'client_id': 'Adxzsj7gA3qqtUqWO493AbfVTnkTQeBWWvTxgnKBxTksPFdU1RWf1UjaDkB_o66d5kg0wdMRzY6K7zJ1',
-    'client_secret': 'EDvZKSak9gRm-uRohkHsN_SrzmGXXwgChjjltKk-NN2Ba4za9hj7fqf5hE7TTk3weYW5tRHBJpKRLuo2'
-  });
-  let amountStored = 0;
-  let currency = ''
-  app.post('/pay', (req, res) => {
-      console.log(req.body)
-      amountStored = req.body.amount
-      currency = req.body.currency
-    const create_payment_json = {
-      "intent": "sale",
-      "payer": {
-          "payment_method": "paypal"
-      },
-      "redirect_urls": {
-          "return_url": `${YOUR_DOMAIN}/paypal/success`,
-          "cancel_url": `${YOUR_DOMAIN}/paypal/cancel`
-      },
-      "transactions": [{
-          "item_list": {
-              "items": [{
-                  "name": req.body.name,
-                  "sku": "001",
-                  "price": amountStored,
-                  "currency": currency,
-                  "quantity": 1
-              }]
-          },
-          "amount": {
-              "currency": currency,
-              "total": amountStored
-          },
-          "description": req.body.description
-      }]
-  };
-  
-  paypal.payment.create(create_payment_json, function (error, payment) {
     if (error) {
-        throw error;
+      res.status(500).end();
     } else {
-        for(let i = 0;i < payment.links.length;i++){
-          if(payment.links[i].rel === 'approval_url'){
-            //res.redirect(payment.links[i].href);
-            console.log("hii")
-            console.log(payment.links[i].href)
-            res.json({forwardLink: payment.links[i].href});
-          }
-        }
+      res.render("store.ejs", {
+        items: JSON.parse(data), //passing items.json data
+        stripePublicKey: stripePublicKey,
+        KEY_ID,
+      });
     }
   });
-  
+});
+
+app.post("/stripe", async (req, res) => {
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ["card"],
+    line_items: [
+      {
+        price_data: {
+          currency: req.body.currency,
+          product_data: {
+            name: req.body.name,
+            images: [req.body.image],
+          },
+          unit_amount: req.body.amount,
+        },
+        quantity: 1,
+      },
+    ],
+    mode: "payment",
+    success_url: `${YOUR_DOMAIN}/stripe/success`,
+    cancel_url: `${YOUR_DOMAIN}/stripe/cancel`,
   });
-  
-  app.get('/paypal/success', (req, res) => {
-    const payerId = req.query.PayerID;
-    const paymentId = req.query.paymentId;
-  
-    const execute_payment_json = {
-      "payer_id": payerId,
-      "transactions": [{
-          "amount": {
-              "currency": currency,
-              "total": amountStored
-          }
-      }]
-    };
-  
-    paypal.payment.execute(paymentId, execute_payment_json, function (error, payment) {
-      if (error) {
-          console.log(error.response);
-          throw error;
-      } else {
-          console.log(JSON.stringify(payment));
-          res.send('Success');
+
+  res.json({id: session.id});
+});
+
+app.get("/stripe/cancel", function (req, res) {
+  res.send("Cancelled");
+});
+app.get("/stripe/success", function (req, res) {
+  res.send("Success");
+});
+
+//PAYPAL
+
+let amountStored = 0;
+let currency = "";
+app.post("/paypal", (req, res) => {
+  amountStored = req.body.amount;
+  currency = req.body.currency;
+  const create_payment_json = {
+    intent: "sale",
+    payer: {
+      payment_method: "paypal",
+    },
+    redirect_urls: {
+      return_url: `${YOUR_DOMAIN}/paypal/success`,
+      cancel_url: `${YOUR_DOMAIN}/paypal/cancel`,
+    },
+    transactions: [
+      {
+        item_list: {
+          items: [
+            {
+              name: req.body.name,
+              sku: "001",
+              price: amountStored,
+              currency: currency,
+              quantity: 1,
+            },
+          ],
+        },
+        amount: {
+          currency: currency,
+          total: amountStored,
+        },
+        description: req.body.description,
+      },
+    ],
+  };
+
+  paypal.payment.create(create_payment_json, function (error, payment) {
+    if (error) {
+      throw error;
+    } else {
+      for (let i = 0; i < payment.links.length; i++) {
+        if (payment.links[i].rel === "approval_url") {
+          //res.redirect(payment.links[i].href);
+          res.json({forwardLink: payment.links[i].href});
+        }
       }
+    }
   });
-  });
-  
-  app.get('/paypal/cancel', (req, res) => res.send('Cancelled'));
+});
 
- 
+app.get("/paypal/success", (req, res) => {
+  const payerId = req.query.PayerID;
+  const paymentId = req.query.paymentId;
 
+  const execute_payment_json = {
+    payer_id: payerId,
+    transactions: [
+      {
+        amount: {
+          currency: currency,
+          total: amountStored,
+        },
+      },
+    ],
+  };
 
-app.get('/stripe/cancel', function(req, res){ 
-    res.send('Cancelled') 
-})
-app.get('/stripe/success', function(req, res){ 
-  res.send('Success') 
-})
+  paypal.payment.execute(
+    paymentId,
+    execute_payment_json,
+    function (error, payment) {
+      if (error) {
+        console.log(error.response);
+        throw error;
+      } else {
+        res.send("Success");
+      }
+    }
+  );
+});
 
-
+app.get("/paypal/cancel", (req, res) => res.send("Cancelled"));
 
 //RAZORPAY
 
+app.post("/razorpay", (req, res) => {
+  //make a req from client and client recives an order_id
+  let options = {
+    amount: req.body.amount, // amount in the smallest currency unit
+    currency: req.body.currency,
+    //receipt: "order_rcptid_11"//we can generate an unique id from npm (like uuid), and generate a recipt everytime you create an order
+  };
+  razorpay.orders.create(options, function (err, order) {
+    res.json(order);
+  });
+});
 
-app.post('/order',(req,res)=>{ //make a req from client and client recives an order_id
-    console.log(req.body)
-    let options = {
-        amount: req.body.amount,  // amount in the smallest currency unit
-        currency: req.body.currency,
-        //receipt: "order_rcptid_11"//we can use an unique idd from npm (like uuid), and generate a recipt everytime you create an order 
-      };
-      razorpay.orders.create(options, function(err, order) {    //create order from the server , specifies the amt u gonna pay and currency (non mutable)
-      //  console.log(order);
-        res.json(order)
-      });
-})
+app.post("/is-razorpay-order-completed", (req, res) => {
+  //console.log(req.body)//you can collect  "razorpay_payment_id" ,"razorpay_order_id","razorpay_signature" from body and use them to verify the signature and Make provisions to store these fields on your server/db.
 
+  razorpay.payments.fetch(req.body.razorpay_payment_id).then((paymentDoc) => {
+    if (paymentDoc.status == "captured") {
+      res.send("Payment success");
+    } else {
+      //res.redirect('/')
+    }
+  });
+});
 
-app.post('/is-order-completed',(req,res)=>{
-   
-    //console.log(req.body)//you can collect  "razorpay_payment_id" ,"razorpay_order_id","razorpay_signature" from body and use them to verify the signature and Make provisions to store these fields on your server/db.
-    
-    razorpay.payments.fetch(req.body.razorpay_payment_id).then((paymentDoc)=>{
-        if(paymentDoc.status=='captured'){
-            console.log('success')
-            res.send('Payment success')
-        }
-        else{
-            //res.redirect('/')
-        }
-    })
-})
-
-
-app.listen(3000,()=>{
-    console.log("Server Started")
-
-})
-
-
-
- 
+app.listen(3000, () => {
+  console.log("Server Started");
+});
